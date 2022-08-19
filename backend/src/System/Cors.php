0f753e2f874a0f31b4cb43fff9b1ddd6c25bfcf9
Yusuf Manshur <ymanshur@gmail.com>
2022-08-19 19:35:29 +0700

    refactor(Cors): change single quote to double quotes

--- backend/src/System/Cors.php
+++ backend/src/System/Cors.php
@@ -7,27 +7,27 @@ class Cors
 
   public function setCors()
   {
-    $this->client_http_origin = 'http://localhost:8080';
+    $this->client_http_origin = "http://localhost:8080";
   
     // Allow from any origin
     if (isset($this->client_http_origin)) {
       // Decide if the origin in $this->client_http_origin is one
       // you want to allow, and if so:
       header("Access-Control-Allow-Origin: {$this->client_http_origin}");
-      header('Access-Control-Allow-Credentials: true');
-      header('Access-Control-Max-Age: 86400');  // cache for 1 day
-      header('Content-Type: application/json');
+      header("Access-Control-Allow-Credentials: true");
+      header("Access-Control-Max-Age: 86400");  // cache for 1 day
     }
 
     // Access-Control headers are received during OPTIONS requests
-    if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
+    if ($_SERVER["REQUEST_METHOD"] == "OPTIONS") {
 
-      if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_METHOD']))
+      if (isset($_SERVER["HTTP_ACCESS_CONTROL_REQUEST_METHOD"]))
         // May also be using PUT, PATCH, HEAD etc
-        header('Access-Control-Allow-Methods: GET, POST, DELETE, OPTIONS');         
+        header("Access-Control-Allow-Methods: GET,POST,DELETE,OPTIONS");         
 
-      if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']))
+      if (isset($_SERVER["HTTP_ACCESS_CONTROL_REQUEST_HEADERS"]))
         header("Access-Control-Allow-Headers: {$_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']}");
+
       exit(0);
     }
   }
