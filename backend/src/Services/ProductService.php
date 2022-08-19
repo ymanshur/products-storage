bbbe93f5cf6b0a2208f9060186be5d16e8e77e92
Yusuf Manshur <ymanshur@gmail.com>
2022-08-19 19:38:01 +0700

    refactor(ProductService): change single quote to double quotes

--- backend/src/Services/ProductService.php
+++ backend/src/Services/ProductService.php
@@ -4,7 +4,7 @@ namespace Src\Services;
 class ProductService
 {
     private $db;
-    private $dbTable = 'product';
+    private $dbTable = "product";
 
     public function __construct($db)
     {
@@ -16,12 +16,8 @@ class ProductService
         $query = "
             SELECT 
                 *
-            FROM (
-                SELECT 
-                    *
-                FROM
-                    " . $this->dbTable . "
-            ) as a
+            FROM
+                " . $this->dbTable . "
             ORDER BY
                 id DESC;
         ";
@@ -37,7 +33,7 @@ class ProductService
 
     public function find($id)
     {
-        $statement = "
+        $query = "
             SELECT 
                 *
             FROM
@@ -46,7 +42,7 @@ class ProductService
         ";
 
         try {
-            $statement = $this->db->prepare($statement);
+            $statement = $this->db->prepare($query);
             $statement->execute(array($id));
             $result = $statement->fetchAll(\PDO::FETCH_ASSOC);
             return $result;
@@ -55,30 +51,30 @@ class ProductService
         }    
     }
 
-    public function insert(Array $input)
+    public function insert(array $input)
     {
         // Sanitize
-        $input['product_sku'] = htmlspecialchars(strip_tags($input['product_sku']));
-        $input['product_name'] = htmlspecialchars(strip_tags($input['product_name']));
-        $input['product_price'] = htmlspecialchars(strip_tags($input['product_price']));
-        $input['product_type'] = htmlspecialchars(strip_tags($input['product_type']));
+        $input["product_sku"] = htmlspecialchars(strip_tags($input["product_sku"]));
+        $input["product_name"] = htmlspecialchars(strip_tags($input["product_name"]));
+        $input["product_price"] = htmlspecialchars(strip_tags($input["product_price"]));
+        $input["product_type"] = htmlspecialchars(strip_tags($input["product_type"]));
 
         // Additional query and sanitize according to product_type
-        $additional_query = '';
-        switch ($input['product_type']) {
-            case 'dvd':
-                $additional_query = 'product_size=:product_size';
-                $input['product_size'] = htmlspecialchars(strip_tags($input['product_size']));
+        $additional_query = "";
+        switch ($input["product_type"]) {
+            case "dvd":
+                $additional_query = "product_size=:product_size";
+                $input["product_size"] = htmlspecialchars(strip_tags($input["product_size"]));
                 break;
-            case 'book':
-                $additional_query = 'product_weight=:product_weight';
-                $input['product_weight'] = htmlspecialchars(strip_tags($input['product_weight']));
+            case "book":
+                $additional_query = "product_weight=:product_weight";
+                $input["product_weight"] = htmlspecialchars(strip_tags($input["product_weight"]));
                 break;
-            case 'furniture':
-                $additional_query = 'product_height=:product_height, product_width=:product_width, product_length=:product_length';
-                $input['product_height'] = htmlspecialchars(strip_tags($input['product_height']));
-                $input['product_width'] = htmlspecialchars(strip_tags($input['product_width']));
-                $input['product_length'] = htmlspecialchars(strip_tags($input['product_length']));
+            case "furniture":
+                $additional_query = "product_height=:product_height, product_width=:product_width, product_length=:product_length";
+                $input["product_height"] = htmlspecialchars(strip_tags($input["product_height"]));
+                $input["product_width"] = htmlspecialchars(strip_tags($input["product_width"]));
+                $input["product_length"] = htmlspecialchars(strip_tags($input["product_length"]));
                 break;
             default:
                 # code...
@@ -96,22 +92,22 @@ class ProductService
             $statement = $this->db->prepare($query);
 
             // Bind values
-            $statement->bindParam(':product_sku', $input['product_sku']);
-            $statement->bindParam(':product_name', $input['product_name']);
-            $statement->bindParam(':product_price', $input['product_price']);
-            $statement->bindParam(':product_type', $input['product_type']);
-            $statement->bindParam(':created_at', date('Y-m-d H:i:s'));
-            switch ($input['product_type']) {
-                case 'dvd':
-                    $statement->bindParam(':product_size', $input['product_size']);
+            $statement->bindParam(":product_sku", $input["product_sku"]);
+            $statement->bindParam(":product_name", $input["product_name"]);
+            $statement->bindParam(":product_price", $input["product_price"]);
+            $statement->bindParam(":product_type", $input["product_type"]);
+            $statement->bindParam(":created_at", date("Y-m-d H:i:s"));
+            switch ($input["product_type"]) {
+                case "dvd":
+                    $statement->bindParam(":product_size", $input["product_size"]);
                     break;
-                case 'book':
-                    $statement->bindParam(':product_weight', $input['product_weight']);
+                case "book":
+                    $statement->bindParam(":product_weight", $input["product_weight"]);
                     break;
-                case 'furniture':
-                    $statement->bindParam(':product_height', $input['product_height']);
-                    $statement->bindParam(':product_width', $input['product_width']);
-                    $statement->bindParam(':product_length', $input['product_length']);
+                case "furniture":
+                    $statement->bindParam(":product_height", $input["product_height"]);
+                    $statement->bindParam(":product_width", $input["product_width"]);
+                    $statement->bindParam(":product_length", $input["product_length"]);
                     break;
                 default:
                     # code...
@@ -127,7 +123,7 @@ class ProductService
 
     public function update($id, array $input)
     {
-        $input['updated_at'] = date('Y-m-d H:i:s');
+        $input["updated_at"] = date("Y-m-d H:i:s");
         
         $params = "";
         foreach ($input as $key => $value) {
@@ -150,7 +146,7 @@ class ProductService
         try {
             $statement = $this->db->prepare($query);
             $statement->execute(array_merge(
-                array('id' => (int) $id),
+                array("id" => (int) $id),
                 $input
             ));
             return $statement->rowCount();
@@ -161,14 +157,14 @@ class ProductService
 
     public function delete($id)
     {
-        $statement = "
+        $query = "
             DELETE FROM " . $this->dbTable . "
             WHERE id = :id;
         ";
 
         try {
-            $statement = $this->db->prepare($statement);
-            $statement->execute(array('id' => (int) $id));
+            $statement = $this->db->prepare($query);
+            $statement->execute(array("id" => (int) $id));
             return $statement->rowCount();
         } catch (\PDOException $e) {
             exit($e->getMessage());
