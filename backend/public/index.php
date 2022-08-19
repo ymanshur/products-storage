a33d6008f105b84c0e81ad9108307c2f87ab58f9
Yusuf Manshur <ymanshur@gmail.com>
2022-08-19 16:52:07 +0700

    fix(index): fix 'Access-Control-Allow-Methods' header

--- backend/public/index.php
+++ backend/public/index.php
@@ -8,9 +8,9 @@ use Src\Controllers\{ProductController};
 
 header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json; charset=UTF-8");
-header("Access-Control-Allow-Methods: OPTIONS,GET,POST,PUT,DELETE");
+header("Access-Control-Allow-Methods: GET,POST,DELETE,PUT,OPTIONS");
 header("Access-Control-Max-Age: 3600");
-header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+header("Access-Control-Allow-Headers: Content-Type,Access-Control-Allow-Headers,Authorization,X-Requested-With");
 
 $cors = new Cors();
 $cors->setCors();
