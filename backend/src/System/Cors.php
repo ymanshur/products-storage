99551a0eee9c3a82b5102081f24b1c660982180f
Yusuf Manshur <ymanshur@gmail.com>
2022-08-22 13:55:01 +0700

    feat(Cors): add 'PUT' at allowed methods header

--- backend/src/System/Cors.php
+++ backend/src/System/Cors.php
@@ -23,7 +23,7 @@ class Cors
 
       if (isset($_SERVER["HTTP_ACCESS_CONTROL_REQUEST_METHOD"]))
         // May also be using PUT, PATCH, HEAD etc
-        header("Access-Control-Allow-Methods: GET,POST,DELETE,OPTIONS");         
+        header("Access-Control-Allow-Methods: GET,POST,PUT,DELETE,OPTIONS");         
 
       if (isset($_SERVER["HTTP_ACCESS_CONTROL_REQUEST_HEADERS"]))
         header("Access-Control-Allow-Headers: {$_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']}");
