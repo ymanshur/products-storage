0b625b51614323716bc1417579c456a187b93a12
Yusuf Manshur <ymanshur@gmail.com>
2022-08-22 14:10:21 +0700

    feat(Controllers): remove validate  method for demo

--- backend/src/Controllers/ProductController.php
+++ backend/src/Controllers/ProductController.php
@@ -87,9 +87,6 @@ class ProductController
             return $this->notFoundResponse();
         }
         $input = (array) json_decode(file_get_contents("php://input"), TRUE);
-        // if (! $this->validateProduct($input)) {
-        //     return $this->unprocessableEntityResponse();
-        // }
         if (! $this->validateProductSKU($input["product_sku"])) {
             return $this->unprocessableEntityResponse();
         }
@@ -112,63 +109,10 @@ class ProductController
     }
 
     private function validateProduct($input)
-    {
-        if (! isset($input["product_sku"])) {
-            return false;
-        }
-        if (! isset($input["product_name"])) {
-            return false;
-        }
-        if (! isset($input["product_price"])) {
-            return false;
-        }
-        if (! isset($input["product_type"])) {
-            return false;
-        }
-        if (
-            ! isset($input["product_size"]) &&
-            ! isset($input["product_weight"]) &&
-            (
-                ! isset($input["product_height"]) &&
-                ! isset($input["product_width"]) &&
-                ! isset($input["product_length"])
-            )
-        ) {
-            return false;
-        }
+    {}
 
-        if (! $this->validateProductSKU($input["product_sku"])) {
-            return false;
-        }
-
-        return true;
-    }
-
-    private function validateProductSKU(string $value) {        
-        $query = "
-            SELECT
-                *
-            FROM (
-                SELECT
-                    *
-                FROM
-                    product
-                WHERE
-                    NOT product_sku=:product_sku
-            ) as a
-            WHERE
-                product_sku=:product_sku;
-        ";
-        $statement = $this->db->prepare($query);
-        $statement->bindParam(":product_sku", $value);
-        $statement->execute();
-        
-        if ($statement->fetchColumn()) {                
-            return false; 
-        }
-
-        return true;
-    }
+    private function validateProductSKU(string $value)
+    {}
 
     private function unprocessableEntityResponse()
     {
