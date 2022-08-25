f445c07df36a0743202953ba1997b7f7c5e8cf0d
Yusuf Manshur <ymanshur@gmail.com>
2022-08-25 17:33:41 +0700

    refactor(index): add comment

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,5 +1,7 @@
 import "regenerator-runtime";
-import "bootstrap/dist/css/bootstrap.min.css";
+
+// Import styles
+import 'bootstrap/dist/css/bootstrap.min.css';
 import "./styles/main.css";
 
 import main from "./scripts/main"
